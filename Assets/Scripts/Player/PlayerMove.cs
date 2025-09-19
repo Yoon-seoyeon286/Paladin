31920c9a78f7de553998eaa594f243bbccbfffb5
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-19 15:29:36 +0900

    feat : script 작성 준비

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
